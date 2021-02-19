9813c865514b651baa0360c179763387fe9e3e0b
Viktorlazi <viktorlazi010@gmail.com>
2021-02-19 11:29:47 +0100

    connect app to blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Web3 from 'web3'
 
 class App extends Component {
+
+  async componentDidMount(){
+    await this.loadWeb3()
+    console.log(window.web3)
+  }
+
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable();
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('non ethereum browser detected')
+    }
+  }
+
   render() {
     return (
       <div>
@@ -20,25 +39,7 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                
               </div>
             </main>
           </div>
