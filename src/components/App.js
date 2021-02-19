fa6f34708333ad244b0a9c904b61dab79ad32ef9
Viktorlazi <viktorlazi010@gmail.com>
2021-02-19 12:25:57 +0100

    identicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import './App.css'
+import Navbar from './Navbar'
 import Web3 from 'web3'
 
 class App extends Component {
-
   async componentDidMount(){
     await this.loadWeb3()
-    console.log(window.web3)
+    await this.loadBlockchainData()
   }
-
+  async loadBlockchainData(){
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account:accounts[0]})
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ethBalance})
+    console.log(this.state.ethBalance)
+  }
   async loadWeb3(){
     if(window.ethereum){
       window.web3 = new Web3(window.ethereum)
@@ -21,20 +28,18 @@ class App extends Component {
       window.alert('non ethereum browser detected')
     }
   }
-
+  constructor(props){
+    super(props)
+    this.state = {
+      account:'',
+      ethBalance:''
+    }
+    
+  }
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account = {this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
